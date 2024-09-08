fc395dc35ddaca79049373bfb01bfcd6530de581
Fırat Alçın <firatalcin04@gmail.com>
2024-09-08 15:49:27 +0300

    Ürün ve Ürünler için gerekli entity'lerin oluşturulması OneToMany | ManyToMany

---
+++ Core/ECommerceAPI.Domain/Entities/Brand.cs
@@ -0,0 +1,18 @@
+﻿using ECommerceAPI.Domain.Common;
+
+namespace ECommerceAPI.Domain.Entities
+{
+    public class Brand : EntityBase
+    {
+        public Brand() { }
+
+        public Brand(string name)
+        {
+            Name = name;
+        }
+
+        public required string Name { get; set; }
+
+       
+    }
+}
---
+++ Core/ECommerceAPI.Domain/Entities/Detail.cs
@@ -0,0 +1,22 @@
+﻿namespace ECommerceAPI.Domain.Entities
+{
+    public class Detail
+    {
+        public Detail()
+        {
+            
+        }
+        public Detail(string title, string description, int categoryId)
+        {
+            Title = title;
+            Description = description;
+            CategoryId = categoryId;
+        }
+
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
